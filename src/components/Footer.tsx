31c1179daff908a956010adccd1241ce25023c68
Jeffrey Chan <jeffreychan1303@gmail.com>
2024-06-14 17:26:48 -0400

    add animation to footer links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,48 +1,68 @@
-import React from 'react';
-import { motion } from 'framer-motion';
+import React, { useRef } from 'react';
+import { motion, useInView } from 'framer-motion';
 
+const parentVariants = {
+  hidden: { opacity: 0 },
+  visible: {
+    transition: {
+      staggerChildren: 0.1
+    }
+  }
+};
+
+const wordVariants = {
+  hidden: { y: 50, rotate: '10deg' },
+  visible: { y: 0, rotate: 0, transition: { duration: 0.5 } }
+};
 const Footer = () => {
+  const inViewRef = useRef(null);
+  const isInView = useInView(inViewRef, { once: false });
+
   return (
-    <footer id='footer'>
-      <div>
+    <motion.footer variants={parentVariants} animate={isInView ? 'visible' : 'hidden'} id='footer'>
+      <div ref={inViewRef} className='footer__overflow-hidden'>
         {/* This will open up a modal page to show credits */}
-        <a className='footer__link'>Credits</a>
+        <motion.a variants={wordVariants} className='footer__link'>
+          Credits
+        </motion.a>
       </div>
       <div>
-        <p style={{ textAlign: 'center' }}>Contact me at </p>
-        <a className='footer__link' href='jeffreychan1303@gmail.com'>
-          jeffreychan1303@gmail.com
-        </a>
+        <div className='footer__overflow-hidden'>
+          <motion.p variants={wordVariants} style={{ textAlign: 'center' }}>
+            Contact me at{' '}
+          </motion.p>
+        </div>
+        <div className='footer__overflow-hidden'>
+          <motion.a variants={wordVariants} className='footer__link' href='jeffreychan1303@gmail.com'>
+            jeffreychan1303@gmail.com
+          </motion.a>
+        </div>
       </div>
       <div className='footer__social-links'>
-        <a className='footer__link' href='https://github.com/JeffreyChan1303' target='_blank' rel='noopener noreferrer'>
-          Github
-        </a>
-        <a
-          className='footer__link'
-          href='https://www.linkedin.com/in/jeffrey-chan-1303/'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Linkedin
-        </a>
+        <div className='footer__overflow-hidden'>
+          <motion.a
+            variants={wordVariants}
+            className='footer__link'
+            href='https://github.com/JeffreyChan1303'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Github
+          </motion.a>
+        </div>
+        <div className='footer__overflow-hidden'>
+          <motion.a
+            variants={wordVariants}
+            className='footer__link'
+            href='https://www.linkedin.com/in/jeffrey-chan-1303/'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Linkedin
+          </motion.a>
+        </div>
       </div>
-      {/* <div style={{ display: 'flex', flexDirection: 'column' }}>
-        <h2>Quick Links</h2>
-        <a>Github</a>
-        <a>LinkedIn</a>
-        <a>My Journey</a>
-        <a>Featured Works</a>
-        <a>Resume</a>
-        <a>Scroll Back Up</a>
-      </div>
-      <div>
-        <a>Send me a message</a>
-        <input type='text' placeholder='Your Name' />
-        <input type='text' placeholder='Your Email' className='contact-me-email' />
-        <textarea placeholder='Your Message'></textarea>
-      </div> */}
-    </footer>
+    </motion.footer>
   );
 };
 
