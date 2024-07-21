7192916e2521648d9bcc1a8e217188069a128a00
Jeffrey Chan <jeffreychan1303@gmail.com>
2024-07-21 19:08:18 -0400

    fix: fix grammer and spelling of experiences

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -53,7 +53,7 @@ const timelineItemArray: Array<{
     subtitle: 'August 2022 - May 2023',
     description: `This was my first research experience that I had. This opportunity was given through UMass Lowell's Immersive Scholars Program. 
       I learned a lot about network security and about the research process. For this research, I utilized Python to build and test algorithms, but also 
-      create plots to analyze the data we found form the different algorithms. I was also able to be part of a paper submission to the Privacy 
+      create plots to analyze the data we found from the different algorithms. I was also able to be part of a paper submission to the Privacy 
       Enhancing Technologies Symposium (PETS).`,
     orientation: TimelineItemOrientation.RIGHT,
     imgSrc: 'network_security_img.png',
@@ -64,7 +64,7 @@ const timelineItemArray: Array<{
     title: 'Athenahealth Software Engineering Intern',
     subtitle: 'June 2023 - August 2023',
     description: `This was my first internship as a software engineer and in a corporate setting. This was a great experience and it was my first time working in a large codebase,
-    in Agile, and with a dedicated team in general. I also learned a lot about the healthcare industry and how software is used in it. I was able to work on the Care Collaboration division whose 
+    in Agile, and with a dedicated team. I also learned a lot about the healthcare industry and how software is used in it. I was able to work in the Care Collaboration division whose 
     goal is to help care managers and care coordinators manage their patients and their care. I mainly used React and Spring boot to build the features I was assigned.`,
     orientation: TimelineItemOrientation.LEFT,
     imgSrc: 'athenahealth-icon.png',
@@ -75,7 +75,7 @@ const timelineItemArray: Array<{
     title: 'Fullstack Developer',
     subtitle: 'November 2023 - May 2024',
     description: `In this position, I was hired to build a project that allowed researchers to collect data for their research through the web. Their 
-    research was about the effects of breaks or rest on productivity when doing a repetitive task. I used Angular, Node.js, and MySQL to build this project. I 
+    research was about the effects of breaks and rest on productivity when doing a task. I used Angular, Node.js, and MySQL to build this project. I 
     was able to help the researchers collect data over the web and aggregated over 30,000 data points for analysis.`,
     orientation: TimelineItemOrientation.RIGHT,
     imgSrc: 'medtask_img.png'
@@ -85,15 +85,15 @@ const timelineItemArray: Array<{
     subtitle: 'January 2024 - May 2024',
     description: `This position was from the UMass Lowell RHSA Roads to Research program, and it allowed me to experience a more business oriented type of research. 
     I worked with a professor to collect and analyze data from twitter to see if we could predict a business's IPO date based on tweet activity. I used Python to 
-    collect 500,000 tweets, filter and aggregated them for us to analyze.`,
+    collect over 500,000 tweets, filtered and aggregated them for us to analyze.`,
     orientation: TimelineItemOrientation.LEFT,
     imgSrc: 'X_logo.svg'
   },
   {
     title: 'MIT Lincoln Laboratory Web Development Co-op',
     subtitle: 'February 2024 - June 2024',
-    description: `This is my second corporate experience and my first time working in a government setting. This was a very interesting environment because 
-    of the security and the type of work that was being done. I was part of the web development team and I was able to work on the internal website for the lab, and the external 
+    description: `This is my second corporate experience and my first time working in the defense contracting industry. This was a very interesting environment because 
+    of the heightened security and the type of work that was being done. I was part of the web development team and I was able to work on both the internal website for the lab, and the external 
     conference websites that the lab would host. I used Drupal, HTML, CSS, and JavaScript to build and maintain these websites. I was also able to learn a lot about the 
     the company and what work a defense contractor company does.`,
     orientation: TimelineItemOrientation.RIGHT,
@@ -105,8 +105,8 @@ const timelineItemArray: Array<{
     title: 'Athenahealth Software Engineering Intern',
     subtitle: 'June 2024 - August 2024',
     description: `This is my second internship at Athenahealth since I loved my first experience so much. This time I was placed is a different team working in a different 
-    repository but was still on the same Care Collaboration division of the company. I utilized React and Nodejs to build features that would help internal care managers and care coordinators 
-    work with their patients and their care.`,
+    repository but was still in the same Care Collaboration division of the company. I utilized React and Nodejs to build features that would help internal care managers and care coordinators 
+    work with their patients and streamline their workflow. `,
     orientation: TimelineItemOrientation.LEFT,
     imgSrc: 'athenahealth-icon.png',
     linkText: 'View Athenahealth website',
